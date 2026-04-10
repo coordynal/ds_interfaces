db73908f00cadddadd4ec6f28f42a03fa2c68d8e
kylewr <kyle@quackings.com>
2026-04-10 00:20:13 -0400

    rename topic_control_change to topic_change_state

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define TOPIC_HEARTBEAT "master_node/heartbeat"
-#define TOPIC_CONTROL_CHANGE "master_node/control_change"
+#define TOPIC_CHANGE_STATE "master_node/change_state"
 #define TOPIC_MASTER_CONNECT "master_node/connect"
 
 #define SERVICE_CHANGE_CONTROL_STATE "master_node/change_control_state"
