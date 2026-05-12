c9aa4c892b1aa63b2d17adbfed51a778c746caf5
kylewr <kyle@quackings.com>
2026-05-12 10:14:06 -0400

    master control updates
    - rename connect.msg to MasterBroadcast.msg
    - add slave-specific control state modification

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -2,10 +2,11 @@
 
 #define TOPIC_HEARTBEAT "master_node/heartbeat"
 #define TOPIC_CHANGE_STATE "master_node/change_state"
-#define TOPIC_MASTER_CONNECT "master_node/connect"
+#define TOPIC_MASTER_BROADCAST "master_node/master_broadcast"
 
 #define SERVICE_CHANGE_CONTROL_STATE "master_node/change_control_state"
 #define SERVICE_CHANGE_CONNECTION "master_node/change_connection"
+#define SERVICE_REQUEST_EDIT_CONNECTION "master_node/request_edit_connection"
 #define SERVICE_REQUEST_SLAVE_LIST "master_node/request_slave_list"
 
 enum ControlState {
